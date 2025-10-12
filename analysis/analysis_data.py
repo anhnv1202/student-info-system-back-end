519026037ae5056dc8658899d5e775e7ea3dc4b4
van <vanntt18588@gmail.com>
2025-10-12 11:15:30 +0700

    clean and analysis data

---
+++ analysis/analysis_data.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_score_box(df, filename):
+    ax = df[['math_score', 'literature_score', 'english_score']].plot.box(figsize=(8, 6))
+    ax.set_title("Phân phối điểm của sinh viên")
+    ax.set_ylabel("Điểm")
+    ax.set_xticklabels(["Điểm Toán", "Điểm Văn", "Điểm Tiếng Anh"])
+    ax.grid(axis='y')
+
+    plt.savefig(filename)
+    plt.show()
+
+def plot_score_scatter(df, file_name):
+    ax = df.plot.scatter(x='math_score', y='english_score', alpha=0.5, figsize=(8, 6))
+    ax.set_title("Tương quan điểm Toán và Tiếng Anh")
+    ax.set_xlabel("Điểm Toán")
+    ax.set_ylabel("Điểm Tiếng Anh")
+    ax.grid(True)
+
+    plt.savefig(file_name)
+    plt.show()
+
+def plot_score_area(df, file_name):
+    score_columns = ['math_score', 'literature_score', 'english_score']
+    new_labels = ["Điểm Toán", "Điểm Văn", "Điểm Tiếng Anh"]
+    axs = df[score_columns].plot.area(figsize=(10, 4), subplots=True)
+    plt.suptitle("Phân phối điểm của sinh viên (Biểu đồ diện tích)")
+    for i, ax in enumerate(axs):
+        handles, _ = ax.get_legend_handles_labels()
+        ax.legend(handles, [new_labels[i]], bbox_to_anchor=(1, 0.7))
+    plt.tight_layout()
+
+    plt.savefig(file_name)
+    plt.show()
+
+def plot_avg_english_by_hometown(df, filename):
+
+    plt.figure()
+    avg_scores = df.groupby('hometown', observed=True)['english_score'].mean().sort_values()
+    ax = avg_scores.plot(kind='barh',
+                         figsize=(12, 8),
+                         color='skyblue')
+    ax.set_title('Điểm Tiếng Anh Trung Bình theo Quê Quán')
+    ax.set_xlabel('Điểm Trung Bình Tiếng Anh')
+    ax.set_ylabel('Quê Quán')
+    ax.grid(axis='x')
+    plt.tight_layout()
+
+    plt.savefig(filename)
+    plt.show()
+
+def plot_math_score_distribution(df, column_name, show_name, filename):
+    bins = range(0, 11)
+    labels = [f'{i}-{i+1}' for i in range(0, 10)]
+    df['temp_column'] = pd.cut(df[column_name], bins=bins, labels=labels, right=False, include_lowest=True)
+
+    score_counts = df['temp_column'].value_counts().sort_index()
+    ax = score_counts.plot(kind='bar', color='skyblue', figsize=(10, 6))
+    ax.set_title('Phổ điểm môn ' + show_name)
+    ax.set_xlabel('Khoảng điểm')
+    ax.set_ylabel('Số lượng học sinh')
+    ax.tick_params(axis='x', rotation=45)
+    ax.grid(axis='y')
+    plt.tight_layout()
+
+    plt.savefig(filename)
+    plt.show()
+
+def analysis_data(input_filepath):
+    img_dir = 'img/'
+    try:
+        # Read file
+        df = pd.read_csv(input_filepath)
+        print("--- Data Analysis ---")
+        print(df.info())
+        print("\nFirst 5 rows of data:")
+        print(df.head())
+
+        # Encode Categorical Variables
+        df['hometown'] = df['hometown'].astype('category')
+
+        # Create plots
+        plot_score_box(df, img_dir + "score_box.png")
+        plot_score_scatter(df, img_dir + "scatter_math_english.png")
+        plot_avg_english_by_hometown(df, img_dir + "english_by_hometown.png")
+        plot_math_score_distribution(df,'math_score', 'Toán', img_dir + 'math_score_distribution.png')
+        plot_math_score_distribution(df,'literature_score', 'Văn', img_dir + 'literature_score_distribution.png')
+        plot_math_score_distribution(df,'english_score', 'Tiếng Anh', img_dir + 'english_score_distribution.png')
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == '__main__':
+    # It is recommended to run main.py
+    # This is for running the script directly from the 'analysis' directory
+    analysis_data("cleaned_student_data.csv")
---
+++ analysis/clean_data.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+def clean_student_data(input_filepath, output_filepath):
+
+    try:
+        # Read file / inspect dataprint / print first 5 rows
+        df = pd.read_csv(input_filepath)
+        print("--- Data Before Cleaning ---")
+        print(df.info())
+        print("\nFirst 5 rows of original data:")
+        print(df.head())
+
+        # Drop column with too many NaNs(> 50% data is NaN)
+        columns_to_drop = df.columns[df.isnull().mean() > 0.5]
+        if not columns_to_drop.empty:
+            print("\nColumns to drop:" + str(columns_to_drop))
+        df.drop(columns=columns_to_drop, inplace=True)
+
+        # Clean string columns by stripping leading/trailing whitespace
+        for col in df.select_dtypes(include=['object']).columns:
+            df[col] = df[col].str.strip()
+
+        # Rename all columns
+        df.rename(columns={'Mã số sinh viên': 'student_code',
+                           'Họ': 'first_name',
+                           'Tên': 'last_name',
+                           'Email': 'email',
+                           'Ngày sinh': 'date_of_birth',
+                           'Quê quán': 'hometown',
+                           'Điểm Toán': 'math_score',
+                           'Điểm Văn': 'literature_score',
+                           'Điểm Tiếng Anh': 'english_score'
+                           }, inplace=True)
+
+        # Clean score columns
+        score_columns = ['math_score', 'literature_score', 'english_score']
+        for col in score_columns:
+            # convert column to numeric, coercing errors into NaN
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+
+            # Convert value not in [0, 10] to NaN
+            valid_number_condition = df[col].between(0, 10)
+            df[col] = df[col].where(valid_number_condition)
+
+            #  Remove row if all scores is NaN
+            df.dropna(subset=score_columns, how='all', inplace=True)
+
+            # Fill missing values (NaN) with the mean of the column
+            # mean_score = df[col].mean()
+            # df[col].fillna(mean_score, inplace=True)
+
+            df[col] = df[col].round(2)
+
+        # Adding new Features: Fullname & Avg_Score
+        df.insert(1, 'full_name', df['first_name'] + ' ' + df['last_name'])
+        df.drop(columns=['first_name', 'last_name'], inplace=True)
+
+        # df['avg_score'] = df[score_columns].mean(axis=1)
+        # df['avg_score'] = df['avg_score'].round(2)
+
+        print("\n--- Data After Cleaning ---")
+        print(df.info())
+        print("\nFirst 5 rows of cleaned data:")
+        print(df.head())
+
+        #  Save to a new CSV file
+        df.to_csv(output_filepath, index=False, encoding='utf-8-sig')
+
+        print(f"\n✅ Data cleaning complete. Cleaned file saved to '{output_filepath}'")
+
+    except FileNotFoundError:
+        print(f"Error: The file '{input_filepath}' was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == '__main__':
+    clean_student_data("danh_sach_sinh_vien.csv", "cleaned_student_data.csv")
+
